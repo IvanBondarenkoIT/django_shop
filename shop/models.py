e6c5ba3f564bec50af99131280480acd748957e7
Ivan <ivan.bondarenko.it@gmail.com>
2023-01-05 22:22:16 +0400

    model product (add) shop.urls (add) shop.views (add)

--- shop/models.py
+++ shop/models.py
@@ -1,6 +1,24 @@
+from django.urls import reverse
 from django.db import models
+
 
 # Create your models here.
 class Product(models.Model):
-    name = models.CharField(max_length=100)
-    slug = models.SlugField()
+    name = models.CharField(max_length=100, db_index=True)
+    slug = models.SlugField(max_length=100, db_index=True)
+    image = models.ImageField(upload_to='products/%Y/%m/%d', blank=True)
+    description = models.TextField(blank=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    available = models.BooleanField(default=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ('name',)
+        index_together = (('id', 'slug'), )
+
+    def __str__(self):
+        return f'{self.name}'
+
+    def get_absolute_url(self):
+        return reverse("shop:product_details", args=[self.id, self.slug])
--- shop/views.py
+++ shop/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Product
 
-# Create your views here.
+
+def product_list(request):
+    products = Product.objects.filter(available=True)
+    return render(request, '', {'products': products})
+
+
+def product_detail(request, id, slug):
+    product = get_object_or_404(Product, id=id, slug=slug, available=True)
+    return render(request, '', {'product': product})
